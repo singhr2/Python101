b410f173cb7bb3f684ffc22d8299be7c77bc3475
Ranbir Singh <ranbir1977@gmail.com>
2017-08-13 21:35:24 +0530

    Additional

--- basics/String2.py
+++ basics/String2.py
@@ -8,6 +8,11 @@ print(sentence)
 print(paragraph)
 
 inputString = input('Enter 1 character')
+
+# In Python, code blocks are denoted by indentation:
+# By convention, most style guides recommend
+# to indent code blocks by four spaces
+
 if inputString == '1':
    print('one')
 elif inputString == '2' :
--- basics/StringTest.py
+++ basics/StringTest.py
@@ -43,26 +43,67 @@ print('string1[:7] ->', string1[:7])
 # either inside " or use \'
 print("doesn\'t", 'doesn\'t')
 
+#With special characters \t, \n
+print('C:\text\new')
+print(r'C:\text\new')
+
+S2 = 'A\nB\tC' # \n is end-of-line, \t is tab
+print('length of ', r'A\nB\tC', 'is ;', len(S2)) # Each stands for just one character
+
+
 # Immutable
+'''
+Every string operation is defined to produce a new string as its result, 
+because strings are immutable in Python—they cannot be changed in place after they are created. 
+In other words, you can never overwrite the values of immutable objects.
+'''
+
 # TypeError: 'str' object does not support item assignment
 # Uncomment below line to see error
 #string1[0] = 'H' # Immutable objects cannot be changed
+
 S='dummy'
 S = 'z' + S[1:] # But we can run expressions to make new objects
 print('s ->', S)
 
+print('abc Is Alphabets :', 'abc'.isalpha())
+print('abc4 Is Alphabets :', 'abc4'.isalpha())
+print('Is Numeric :', 'abc4'.isnumeric())
+print('Is digit :', 'abc4'.isdigit())
+print('Is AlphaNumeric :', 'abc4'.isalnum())
+
 
 #'in' operator in Strings
 str2='Change'
-print('c' in str2)
-print('C' in str2)
+print('Is \'c\' present in ', str2,  'c' in str2)
+print('Is \'C\' present in ', str2,  'C' in str2)
 
 ## Find the offset of a substring in S
-print("str2.find('ang') ->", str2.find('ang'))
+print("Change.find('ang') ->", str2.find('ang'))
 
+## Replace
 str2=str2.replace('ang', 'XYZ')
 print('str2 ->', str2)
 
+# String to List
+line = 'aaa,bbb,ccccc,dd'
+myList = line.split(',')
+print('myList =', myList)
+totalItemsInList = len(myList);
+print('Total items in list:' , totalItemsInList)
+print('Last item in list :', myList[totalItemsInList-1])
+
+
+# dir: lists variables assigned in the caller’s scope
+# Because methods are function attributes, they will show up in this list.
+# names with double underscores in this list  relates to overloading in classes
+# they represent the implementation
+print(dir())
+print(dir(myList))
+
+## Help on topic
+print(help(dir))
+print(help(S.replace))
 
 #
 #The backslash (\) character is used to introduce a special character. See the following table.
