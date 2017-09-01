dcc9c9969712fa7cbd1caf23df0e9799ab7a6888
Ranbir Singh <ranbir.singh@cognizant.com>
2017-09-01 09:38:05 +0530

    updated examples

--- basics/CollectionType.py
+++ basics/CollectionType.py
@@ -18,33 +18,101 @@ type([]) # <class 'list'>
 type({}) # <class 'dict'>
 type(()) # <class 'tuple'>
 
-l1=['1','2']
-type(l1) # <class 'list'>
-print(l1, ' has ', len(l1), 'elements') # ['1', '2']  has  2 elements
-print('list.count(\'2\') :', l1.count('2')) # 1
-print('list.count(\'-999\') :', l1.count('-999')) # 0
-print('l1.index(\'2\') ?', l1.index('2')) # 1
-#print('l1.index(\'-999\') ?', l1.index('-999')) # ValueError: '-999' is not in list
+# -----------------------------------
+#  List functions
+# -----------------------------------
+myList1=['1','2']
+type(myList1) # <class 'list'>
+print(myList1, ' has ', len(myList1), 'elements') # ['1', '2']  has  2 elements
+
+print('list.count(\'2\') :', myList1.count('2')) # 1
+print('list.count(\'-999\') :', myList1.count('-999')) # 0
+
+# index() : Raises a ValueError if there is no such item.
+print('myList1.index(\'2\') ?', myList1.index('2')) # 1
+#print('myList1.index(\'-999\') ?', myList1.index('-999')) # ValueError: '-999' is not in list
 
 #Add element at end
-l1.append('3rd') # ['1', '2', '3rd']
-print(l1)
+myList1.append('3rd') # ['1', '2', '3rd']
+print(myList1)
 
 #add element at index
-l1.insert(1, 'add at index 1') #['1', 'add at index 1', '2', '3rd']
-print(l1)
+desired_index= 1;
+myList1.insert(desired_index, 'add at index 1') #['1', 'add at index 1', '2', '3rd']
+print(myList1)
 
-l1.remove('add at index 1')
-print(l1)
+# Removes the first item from the list that has a value of x. 
+#don't return it.
+myList1.remove('add at index 1')
+print(myList1)
+
+# Removes the item at the given position in the list, and returns it. 
+#If no index is specified, pop() removes and returns the last item in the list.
+index_to_remove = 2;
+myList1.pop(index_to_remove)
+print('List after removing element at index (',index_to_remove,') :', myList1)
+
+# remove all elements from list
+myList1.clear()
+print('List after removing all elements :', myList1)
+
+# Iterate through list
+myList1.append('temp1')
+myList1.append('temp2')
+myList1.append('temp3')
+
+print(myList1)
+
+for i in myList1 :
+	print(i)
+
+# extend list
+list1 = ['physics', 'chemistry', 'maths']
+list2=list(range(5)) #creates list of numbers between 0-4
+list1.extend(list2)
+print ('list1 :', list1)  # ['physics', 'chemistry', 'maths', 0, 1, 2, 3, 4]
+print ('list2 :', list2)  # [0, 1, 2, 3, 4]
+
+# reverse list
+list1.reverse()
+print ('list1 (after reverse) :', list1)
+
+# SORTING LIST
+# sort(key=None(default), reverse=False(default))
+
+#Sorts objects of list, use compare func if given
+a = ["bee", "wasp", "moth", "ant"]
+a.sort()
+print(a) #['ant', 'bee', 'moth', 'wasp']
+
+a = ["bee", "butterfly", "wasp", 'a']
+a.sort(key=len)
+print(a) #['a', 'bee', 'wasp', 'butterfly']
+
+a = ["bee", "butterfly", "wasp", 'a']
+a.sort(key=len, reverse=True)
+print(a) #['butterfly', 'wasp', 'bee', 'a']
+
+
+#
+S = {c * 4 for c in 'spam'}
+print(S) # {'pppp', 'ssss', 'aaaa', 'mmmm'}
+
 
 # ------------------------------------
 t1=('1','2') 
 type(t1) #<class 'tuple'>
 
+
+
+
 # ------------------------------------
 # SET
 # sets are essentially like valueless dictionaries
-#set’s items are unordered, unique, and immutable
+# set’s items are unordered, unique, and immutable
+# 
+#largely because of their implementation, sets can only contain immutable (a.k.a. “hashable”) object types. 
+# Hence, lists and dictionaries cannot be embedded in sets, but tuples can if you need to store compound values.
 
 s2={'1','2'}
 type(s2) # <class 'set'>
@@ -53,10 +121,15 @@ set('spam') #{'m', 's', 'p', 'a'}
 
 type([1, 2, 3, 4]) # <class 'list'>
 # Create Set from List
+L = [1, 2, 1, 3, 2, 4, 5]
+s1=set(L)
+print('set from list:', s1) # {1, 2, 3, 4, 5}
+
+{1, 2, 3, 4, 5}
+
 set([1, 2, 3, 4]) # {1, 2, 3, 4}
 
 type({1, 2, 3, 4}) #<class 'set'>
-
 
 S1 = {1, 2, 3, 4}
 print('S1 :', S1)
@@ -78,6 +151,40 @@ S1 - {1, 2, 3, 4} # set() , { Note: Empty sets print differently }
 # add() method
 S1.add('alot')
 print('S1 (after adding alot :', S1) #{1, 2, 3, 4, 'alot'}
+
+
+# Comparison
+L1, L2 = [1, 3, 5, 2, 4], [2, 5, 3, 4, 1]
+L1 == L2 # False, Order matters in sequences
+
+set(L1) == set(L2) # True, Order-neutral equality
+
+sorted(L1) == sorted(L2) # True, Similar but results ordered
+
+'spam' == 'asmp', set('spam') == set('asmp'), sorted('spam') == sorted('asmp')
+#(False, True, True)
+
+
+engineers = {'bob', 'sue', 'ann', 'vic'}
+managers = {'tom', 'sue'}
+'bob' in engineers # Is bob an engineer?
+# True
+
+engineers & managers # Who is both engineer and manager?
+#{'sue'}
+
+engineers | managers # All people in either category
+#{'bob', 'tom', 'sue', 'vic', 'ann'}
+
+engineers - managers # Engineers who are not managers
+#{'vic', 'ann', 'bob'}
+managers - engineers # Managers who are not engineers
+#{'tom'}
+
+engineers > managers # Are all managers engineers? (superset)
+#False
+
+
 
 # ------------------------------------
 d2={'1':'one', '2':'two'}
--- basics/HellowWorld.py
+++ basics/HellowWorld.py
@@ -1,3 +1,8 @@
+#HellowWorld.py
+# to run, 
+# cd  D:\dev\python\Python101\basics
+# python HellowWorld.py
+
 print('Hello World')
 
 #When two or more arguments are passed, print  function displays each argument separated by space.
@@ -24,6 +29,21 @@ print('y=', y)
 
 print('type(x) :', type(x))
 
+# you can assign multiple variables within a single statement.
+a, b, c = 1, 2, "Hey"
+
+#assign the same value to multiple variables
+a = b = c = "Hey"
+
+# Mix
+#a, b = 2  #TypeError: 'int' object is not iterable
+
+#variable names (as with all identifiers) are case sensitive.
+var1='all lowercase'
+vAr1='A capital'
+vaR1='R capital'
+vAR1='AR caps'
+print(var1, vAr1, vaR1, vAR1)
 
 # Receiving input from Console
 name = input("Enter your name: ")
@@ -50,6 +70,12 @@ print('3//2=', 3//2)
 
 #  remainder or modulus operator
 print('35%4=', 35%4)
+
+
+# you can treat True and False as though
+# they are predefined variables set to integers 1 and 0.
+True == 1
+
 
 # False and True will have 1st char as Caps
 x=False
