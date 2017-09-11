b9e4d1f81f199b90666bb27588c61f760865a7f7
Ranbir Singh <ranbir.singh@cognizant.com>
2017-09-11 10:00:41 +0530

    updates

--- basics/Files.py
+++ basics/Files.py
@@ -8,7 +8,13 @@
 # > python Files.py
 
 '''
-> 'r' (read) is the default processing mode
+! Must have exactly one of create/read/write/append mode and at most one plus
+> 'r' (read) : default processing mode
+> 'w' : write
+> 'a' : 'a' to open for appending text to the end (e.g., for adding to logfiles).
+> 'rb': Python 3.X bytes files (bytes strings)
+> Adding a + opens the file for both input and output (i.e., you can both read and
+write to the same file object,
 
 > Files work in two modes:
   a) text, which represents content as str and implements Unicode encodings, and
@@ -16,7 +22,6 @@
      binary files are useful for processing media, accessing data created by C programs, and so on.
      Python’s struct module can both create and unpack packed binary data—raw bytes
      that record values that are not Python objects—to be written to a file in binary mode.
-
 '''
 
 
@@ -53,6 +58,40 @@ below line will produce following error=> OSError: [Errno 22] Invalid argument: 
 
 If the letter r (uppercase or lowercase) appears just before the opening quote of a string, it turns off the escape mechanism.
     myfile = open(r'C:\new\text.dat', 'w')
-Or keep your backslashes by simply doubling them 
+Or keep your backslashes by simply doubling them
     myfile = open('C:\\new\\text.dat', 'w')
 '''
+
+fileToRead = open('_Git Commands.txt', 'r')
+print('\n >>> reading 6 chars [', fileToRead.read(6), ']')
+print() #Read up to next N characters (or bytes) into a string
+
+print('>>> Reading next line (including \\n newline) into a string')
+aString = fileToRead.readline()
+print(aString)
+
+# This will read from where previous command has left.
+print('\n >>> read() - Read entire file into a single string')
+print(fileToRead.read())  # Read entire file into a single string
+
+#Change file position to offset N for next operation
+fileToRead.seek(0)
+
+print('\n >>> readlines() - ', fileToRead.readlines())  # Read entire file into list of line strings (with \n)
+
+
+#use line File iterators read line by line
+for line in open('cls.py'):
+    print(line)
+
+#Flush output buffer to disk without closing
+#someFileName.flush()
+
+#Manual close (done for you when file is collected)
+fileToRead.close()
+
+#  + Not working 
+fileToRead2 = open('DummyFile.txt', 'w+')
+fileToRead2.writelines('lineXyZ')
+fileToRead2.flush()
+print(fileToRead2.readlines())
--- basics/MiscExamples.py
+++ basics/MiscExamples.py
@@ -5,3 +5,44 @@
 dir(dict) # dict is the name of the type
 
 help(dict)
+
+
+#!/usr/bin/env python3
+
+'''
+On Unix, when a program is invoked in the console, the file’s first two bytes are read.
+* If these bytes are the ASCII characters #!, the shell assumes that the file is to be executed by an interpreter 
+and that the file’s first line specifies which interpreter to use. 
+This line is called the shebang (shell execute) line, and if present must be the first line in the file.
+'''
+
+'''
+to redirect the output of this program to file,
+	python eg01.py > eg01-output.txt
+'''
+
+import os # os Module 
+print('Current Working Directory :', os.getcwd())
+
+print('~~~~~~~~')
+
+print("Hello", "World!")
+print('hello world')
+print(2 ** 4)
+print('Spam!' * 8) # Repeats Spam! 8 times
+
+print('~~~~~~~')
+for x in 'ranbir':
+	print(x)
+	print('---')
+print('Exited loop')
+
+print('~~~~~~~~')
+import sys
+print('Platform:'+ sys.platform)
+print('Platform:', sys.platform)
+
+
+print('~~~~~~~')
+name=input('enter your name:')
+print('you have entered ->', name)
