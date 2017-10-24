f100d7320ff65aa07bcb25947215d90cf686ccde
Ranbir Singh <ranbir.singh@cognizant.com>
2017-10-24 13:29:26 +0530

    .

--- basics/MapReduceFilter.py
+++ basics/MapReduceFilter.py
@@ -1,4 +1,4 @@
-#MapReduceFilter.py
+# MapReduceFilter.py
 
 # map() function
 #   => Mapping Functions over Iterables: map
@@ -6,6 +6,9 @@
 
 # The map function applies a passed-in function to each item in an iterable object 
 # and returns a list containing all the function call results.
+#
+# map applies a 'function' call to each item instead of an arbitrary 'expression', 
+# it is a somewhat less general tool, and often requires extra helper functions or lambdas.
 
 '''
 map calls funcIncrementBy10 on each list item
@@ -19,10 +22,17 @@ inputList = [1, 2, 3, 4]
 def funcIncrementBy10(x): 
 	return x + 10
 
-print(list(map(funcIncrementBy10, inputList))) # [11, 12, 13, 14]
+# Output : [11, 12, 13, 14]
+print(list(map(funcIncrementBy10, inputList))) 
 
 
-#--
-# Function expression
+# (Option-2) using list comprehension
+print(list(funcIncrementBy10(x) for x in inputList))
+
+print( [funcIncrementBy10(x) for x in inputList] )
+print( {funcIncrementBy10(x) for x in inputList} )
+
+#
+# (Option-3) Using Lambda instead of function
 # Output : [4, 5, 6, 7]
 print(list(map((lambda x: x + 3), inputList))) 
