97164e856bb8fdd082b35d19d959edba5d51f64b
Ranbir Singh <ranbir.singh@cognizant.com>
2017-11-06 17:56:04 +0530

    updated code

--- basics/Class101.py
+++ basics/Class101.py
@@ -1,15 +1,19 @@
 # Class101.py
 
 '''
+classes are essentially factories for generating multiple instances
+
 A class consists of two parts: the header and the body. 
 
- The instances possess dictionaries __dict__, which they use to store their attributes and their corresponding values.
+Python’s class model is extremely dynamic. 
+Classes and instances are just namespace objects, with attributes created on the fly by assignment.
 
- Python automatically calls a method named __init__ each time an instance is generated from a class
- The new instance is passed in to the self argument of __init__ as usual
- ! The __init__ method is known as the constructor because of when it is run
- If no __init__ is present, class calls return an empty instance, without initializing it.
+The instances possess dictionaries __dict__, which they use to store their attributes and their corresponding values.
 
+Python automatically calls a method named __init__ each time an instance is generated from a class
+The new instance is passed in to the self argument of __init__ as usual
+! The __init__ method is known as the constructor because of when it is run
+If no __init__ is present, class calls return an empty instance, without initializing it.
 '''
 
 '''
@@ -17,20 +21,27 @@ class DerivedClassName(BaseClassName):
     pass
 '''
 
+def convertToUppercase(someObj):
+	return someObj.name.upper()
+
 class Person:
 
-    def __init__(self, first, last):
-        self.firstname = first
+    def __init__(self, first, last): # class header
+        self.firstname = first # members / attributes
         self.lastname = last
 
     def Name(self):
         return self.firstname + " " + self.lastname
 
+
+# class Employee extends Person
 class Employee(Person):
 
-    def __init__(self, first, last, staffnum):
+
+	# If no __init__ is present, class calls return an empty instance, without initializing it.
+    def __init__(self, first, last, staffnum): # constructor
         Person.__init__(self,first, last) 
-        # can be -> 
+        # it can also be coded as -> 
         #super().__init__(first, last) 
 
         self.staffnumber = staffnum
@@ -38,9 +49,35 @@ class Employee(Person):
     def GetEmployee(self):
         return self.Name() + ", " +  self.staffnumber
 
+#Each instance object inherits class attributes and gets its own namespace.
 x = Person("Ranbir", "Singh")
 y = Employee("James", "Bond", "1007")
 
 print('-------------------')
 print(x.Name())
 print(y.GetEmployee())
+
+
+z = Employee("Arnold", "LName", 123.45) #
+
+#polymorphism 
+#means that the meaning of an operation depends on the object being operated on. 
+print(z.staffnumber)
+
+
+
+class MyEmptyClass: pass # Empty namespace object
+# classes are objects in their own right, even without instances.
+MyEmptyClass.name = 'Bob' # Just objects with attributes
+print(MyEmptyClass.name) # Note: Acessing by class name w/o creating object
+x = MyEmptyClass() # Instance-based records
+y = MyEmptyClass()
+
+# name is stored on the class only
+print('before :', MyEmptyClass.name, x.name, y.name) # prints -> before : Bob Bob Bob
+x.name = 'Ranbir'
+print('after :', MyEmptyClass.name, x.name, y.name) # prints -> after : Bob Ranbir Bob
+
+
+
+print(convertToUppercase(x))
